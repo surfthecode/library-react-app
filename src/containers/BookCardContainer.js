4010dbcef57b6b8ef5d00f7e8a22ca44bd54c3b6
surfthecode <surfthecode@gmail.com>
2023-08-08 17:11:11 +0100

    book container now shows book card

--- src/containers/BookCardContainer.js
+++ src/containers/BookCardContainer.js
@@ -3,7 +3,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
 
-import BookCard from "./components/BookCard";
+import BookCard from "../components/BookCard";
 
 const BookCardContainer = () => {
   return (
